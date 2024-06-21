7b7c25616b57892647559d1c713d89af102e7fa5
kvn <kvnareba@gmail.com>
2024-06-21 08:12:53 +0300

    comp

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -11,8 +11,8 @@ def test_python_version():
 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
+    assert requests_version() == "2.25.1"
 
 
 def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    assert pytest_version() == "8.2.2"
